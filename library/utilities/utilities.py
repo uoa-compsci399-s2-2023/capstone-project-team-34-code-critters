d6efee1bd19aca20e9c72899ead3bfeac8c1021c
JCHU634 <87676426+jchu634@users.noreply.github.com>
2023-07-29 23:27:49 +1200

    Stripping to the bare minimum

--- library/utilities/utilities.py
+++ library/utilities/utilities.py
@@ -9,10 +9,6 @@ from library.utilities.standardise_images import standardise_images
 utilities_blueprint = Blueprint(
     'utilities_bp', __name__)
 
-
-# def get_search_url():
-#     return url_for('search_bp.search_interface')
-
 def get_home_url():
     return url_for('home_bp.home')
 
@@ -24,24 +20,3 @@ def upload_files():
         f.save('/path/to/save/' + filename)
         standardise_images(filename)
         return 'File uploaded successfully'
-    
-    
-
-# def get_list_url():
-#     return url_for('book_bp.list_books')
-
-# def get_register_url():
-#     return url_for('authentication_bp.register')
-
-# def get_login_url():
-#     return url_for('authentication_bp.login')
-
-# def get_logout_url():
-#     return url_for('authentication_bp.logout')
-
-# def get_selected_articles(quantity=3):
-#     articles = services.get_random_articles(quantity, repo.repo_instance)
-
-#     for article in articles:
-#         article['hyperlink'] = url_for('news_bp.articles_by_date', date=article['date'].isoformat())
-#     return articles
