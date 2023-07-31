2931e41e140629f870ce5e2a6349257690fca450
JCHU634 <87676426+jchu634@users.noreply.github.com>
2023-07-31 23:59:04 +1200

    Updated env variables

--- backend/library/__init__.py
+++ backend/library/__init__.py
@@ -4,28 +4,21 @@ from flask import Flask, url_for, request,send_from_directory
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config.from_object('config.Config')
-
+    # app.config.from_object('config.Config')
+    app.config["DIR_PATH"] = os.path.dirname(os.path.realpath(__file__))
+    app.config["UPLOAD_FOLDER"] ='./backend/library/static/uploads/'
+    app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
+    
     # if test_config is not None:
     #     app.config.from_mapping(test_config)
-    #     tests = True
-    #     # populate(repo.repo_instance,"tests")
     # else:
-    #     # populate(repo.repo_instance)
     #     tests = False
     
     with app.app_context():
-    #     from .books import book
-    #     app.register_blueprint(book.book_blueprint)
 
         from .home import home
         app.register_blueprint(home.home_blueprint)
 
-    #     from .search import search
-    #     app.register_blueprint(search.search_blueprint)
-
-    #   from .authentication import authentication
-    #   app.register_blueprint(authentication.authentication_blueprint)
 
         from .utilities import utilities
         app.register_blueprint(utilities.utilities_blueprint)
