e094953b81e34ab36b951fac8cd28c6a40ae9e5f
JCHU634 <87676426+jchu634@users.noreply.github.com>
2023-08-08 01:58:03 +1200

    Adjusted preprocessing slightly for accuracy Moved all preprocessing to function

--- backend/library/utilities/inference.py
+++ backend/library/utilities/inference.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 import imp
 
+
 current_model = "trupanea_v2"
 path = current_app.config["DIR_PATH"] + "/models/" + current_model
 isProduction = os.getenv('FLASK_ENV') == 'production'
@@ -25,24 +26,19 @@ def get_prediction(image_path):
     model = load_model(model_path)
     labels = get_labels(current_model)
     
+    # Preprocess the image
     preprocess = imp.load_source('img_preprocess', f'{path}/preprocess.py')
-    # Convert the image from PIL format to the OpenCV BGR format
-    img = np.array(preprocess.img_preprocess(image_path))[:,:,::-1]
-
-    # Add a batch dimension to the image
-    img = np.expand_dims(img, axis=0)
-    
-    # Normalize the image
-    # img = img / 255.0
+    img = preprocess.img_preprocess(image_path)
 
     # Get the prediction from the model
     prediction = model.predict(img)
+
     # Combine the labels and the predictions
     combined_list = list(zip(prediction[0], labels))
 
     # Sort the list in descending order based on the predictions
     sorted_list = sorted(combined_list, key=lambda x: x[1], reverse=True)
 
-    #return the top 20% of predictions
-    return [[float(prob),label] for prob,label in sorted_list]
+    # Return all predictions (Formatted as Strings without scientific notation)
+    return [[f'{prob:.20f}',label] for prob,label in sorted_list]
     
