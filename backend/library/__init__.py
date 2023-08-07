c846f059160a06c5eeb79ab2e959f06fe734b497
JCHU634 <87676426+jchu634@users.noreply.github.com>
2023-08-08 02:55:42 +1200

    Added XLSX export endpoint and registered it

--- backend/library/__init__.py
+++ backend/library/__init__.py
@@ -9,7 +9,7 @@ def create_app(test_config=None):
     # app.config.from_object('config.Config')
     app.config["DIR_PATH"] = os.path.dirname(os.path.realpath(__file__))
     app.config["UPLOAD_FOLDER"] ='./library/static/uploads/'
-    app.config["DOWNLOAD_FOLDER"] ='./library/static/downloads/'
+    app.config["STORAGE_FOLDER"] ='.\library\static\storage\\'
 
     app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
     
@@ -23,7 +23,7 @@ def create_app(test_config=None):
         app.register_blueprint(home.home_blueprint)
 
 
-        from .utilities import utilities, create_xlsx
+        from .utilities import utilities, xlsx_export
         app.register_blueprint(utilities.utilities_blueprint)
 
         blueprint = Blueprint('api', __name__, url_prefix='/api')
@@ -31,7 +31,7 @@ def create_app(test_config=None):
         
         app.register_blueprint(blueprint)
         api.add_namespace(utilities.utils_api)
-        api.add_namespace(create_xlsx.utils_api)
+        api.add_namespace(xlsx_export.utils_api)
         
     return app
 
--- backend/library/utilities/xlsx_export.py
+++ backend/library/utilities/xlsx_export.py
@@ -1,18 +1,30 @@
 
 from openpyxl import Workbook
+from openpyxl.drawing.image import Image
 
-from flask import current_app, request
-from flask_restx import Namespace, Resource, fields
+from flask import current_app, request, send_from_directory, send_file
+from flask_restx import Namespace, Resource
 
 from library.utilities.standardise_images import standardise_image
 from library.utilities.inference import get_prediction
+from PIL import Image as PILimg
+from PIL import ImageOps
+import os
+
+import hashlib
 
 ################ Blueprint/Namespace Configuration ################
 utils_api = Namespace('utilities_api', description='Utilities related operations')
 json_parser = utils_api.parser()
-json_parser.add_argument('file[]', location='json', type=list, required=True, help='Results JSON')
+json_parser.add_argument('results', location='json', type=list, required=True, help='Results JSON')
+
+def resize_img(path):
+    img = PILimg.open(path)
+    img = ImageOps.fit(img, (100, 100), method=PILimg.Resampling.LANCZOS)
+    img.save(path)
 
 ################ Global Variables ################
+storage_path = current_app.config['STORAGE_FOLDER']
 img_path = current_app.config['UPLOAD_FOLDER']
 
 ################ API Endpoints ################
@@ -21,24 +33,53 @@ img_path = current_app.config['UPLOAD_FOLDER']
 class upload_files_json(Resource):
     
     @utils_api.response(200, 'Success')
+    @utils_api.response(405, 'Invalid File Type')
     @utils_api.expect(json_parser)
     def post(self):
         if request.is_json:
             json_data = request.get_json()
-            wb = Workbook()
-            ws = wb.active
-            ws.append(["Filename", "Prediction"])
-            for file in json_data["file[]"]:
-                filename = file["name"]
-                pred = file["pred"]
-                ws.append([filename, pred])
-            wb.save("results.xlsx")
+            # File name is based on the hash of the JSON data
+            str_data = str(json_data)
+            hash_object = hashlib.md5(str_data.encode())
+            hash_hex = hash_object.hexdigest()
+
+            # Create the file if it doesn't exist
+            if not os.path.isfile(storage_path+hash_hex+".xlsx"):
+                wb = Workbook()
+                ws = wb.active
+                ws.append(["Filename","Image", "Predictions"])
+                for result in json_data:
+                    filename = result["name"]
+                    pred = [item for subpred in result["pred"] for item in subpred]
+                    ws.append([filename]+[""]+pred)
+                    if os.path.isfile(img_path+filename):
+                        resize_img(img_path+filename)
+
+                        img = Image(img_path+filename)
+                        ws.add_image(img, "B"+str(ws.max_row))
+                        # ws.append([filename]+[""]+pred)
+                
+                #Auto adjust column width A (FILENAME)
+                max_length = 0
+                for cell in ws['A']:
+                    try:
+                        if len(str(cell.value)) > max_length:
+                            max_length = len(str(cell.value))
+                    except:
+                        pass
+                adjusted_width = (max_length + 2) * 1.2
+                ws.column_dimensions['A'].width = adjusted_width
+
+                # Adjust column width B (IMAGE)
+                ws.column_dimensions['B'].width = 14 # Found via trial and error
+                for row in ws.iter_rows(min_row=2):
+                    ws.row_dimensions[row[0].row].height = 80 # Found via trial and error
+
+                wb.save(storage_path+hash_hex+".xlsx")
             
-
-
-    
-                    
-            return "Success", 200
+            path = os.path.join(current_app.static_folder, 'storage/') # Yes I know this is inconsistent, This is the only way i could get this to work
+            return send_file(path+hash_hex+".xlsx", as_attachment=True)
+            # return send_from_directory(path, hash_hex+".xlsx")
         else:
             return "Invalid JSON", 405
         
