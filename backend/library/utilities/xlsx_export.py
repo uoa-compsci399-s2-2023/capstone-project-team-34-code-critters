5fdbf137647af9e15d24a611e7507c2a26d21cc9
JCHU634 <87676426+jchu634@users.noreply.github.com>
2023-08-09 01:10:13 +1200

    [NEW] XLSX Export Error Handling + More verbose HTTP Return Codes

--- backend/library/utilities/xlsx_export.py
+++ backend/library/utilities/xlsx_export.py
@@ -32,55 +32,64 @@ img_path = current_app.config['UPLOAD_FOLDER']
 class XLSX_export(Resource):
     
     @utils_api.response(200, 'Success')
-    @utils_api.response(405, 'Invalid File Type')
+    @utils_api.response(400, 'Bad Request (Likely Invalid JSON)')
+    @utils_api.response(500, 'Internal Server Error')
     @utils_api.expect(json_parser)
     def post(self):
-        if request.is_json:
-            json_data = request.get_json()
-            # File name is based on the hash of the JSON data
-            str_data = str(json_data)
-            hash_object = hashlib.md5(str_data.encode())
-            hash_hex = hash_object.hexdigest()
+        try:
+            if request.is_json:
+                json_data = request.get_json()
 
-            # Create the file if it doesn't exist
-            if not os.path.isfile(storage_path+hash_hex+".xlsx"):
-                wb = Workbook()
-                ws = wb.active
-                ws.append(["Filename","Image", "Predictions"])
-                for result in json_data:
-                    filename = result["name"]
-                    pred = [item for subpred in result["pred"] for item in subpred]
-                    ws.append([filename]+[""]+pred)
-                    if os.path.isfile(img_path+filename):
-                        resize_img(img_path+filename)
-
-                        img = Image(img_path+filename)
-                        ws.add_image(img, "B"+str(ws.max_row))
-                        # ws.append([filename]+[""]+pred)
+                for obj in json_data:
+                    if "name" not in obj or "pred" not in obj:
+                        return "Invalid JSON", 400
                 
-                #Auto adjust column width A (FILENAME)
-                max_length = 0
-                for cell in ws['A']:
-                    try:
-                        if len(str(cell.value)) > max_length:
-                            max_length = len(str(cell.value))
-                    except:
-                        pass
-                adjusted_width = (max_length + 2) * 1.2
-                ws.column_dimensions['A'].width = adjusted_width
+                # File name is based on the hash of the JSON data
+                str_data = str(json_data)
+                hash_object = hashlib.md5(str_data.encode())
+                hash_hex = hash_object.hexdigest()
 
-                # Adjust column width B (IMAGE)
-                ws.column_dimensions['B'].width = 14 # Found via trial and error
-                for row in ws.iter_rows(min_row=2):
-                    ws.row_dimensions[row[0].row].height = 80 # Found via trial and error
+                # Create the file if it doesn't exist
+                if not os.path.isfile(storage_path+hash_hex+".xlsx"):
+                    wb = Workbook()
+                    ws = wb.active
+                    ws.append(["Filename","Image", "Predictions"])
+                    for result in json_data:
+                        filename = result["name"]
+                        pred = [item for subpred in result["pred"] for item in subpred]
+                        ws.append([filename]+[""]+pred)
+                        if os.path.isfile(img_path+filename):
+                            resize_img(img_path+filename)
 
-                wb.save(storage_path+hash_hex+".xlsx")
-            
-            path = os.path.join(current_app.static_folder, 'storage/') # Yes I know this is inconsistent, This is the only way i could get this to work
-            return send_file(path+hash_hex+".xlsx", as_attachment=True)
-            # return send_from_directory(path, hash_hex+".xlsx")
-        else:
-            return "Invalid JSON", 405
+                            img = Image(img_path+filename)
+                            ws.add_image(img, "B"+str(ws.max_row))
+                            # ws.append([filename]+[""]+pred)
+                    
+                    #Auto adjust column width A (FILENAME)
+                    max_length = 0
+                    for cell in ws['A']:
+                        try:
+                            if len(str(cell.value)) > max_length:
+                                max_length = len(str(cell.value))
+                        except:
+                            pass
+                    adjusted_width = (max_length + 2) * 1.2
+                    ws.column_dimensions['A'].width = adjusted_width
+
+                    # Adjust column width B (IMAGE)
+                    ws.column_dimensions['B'].width = 14 # Found via trial and error
+                    for row in ws.iter_rows(min_row=2):
+                        ws.row_dimensions[row[0].row].height = 80 # Found via trial and error
+
+                    wb.save(storage_path+hash_hex+".xlsx")
+                
+                path = os.path.join(current_app.static_folder, 'storage/') # Yes I know this is inconsistent, This is the only way i could get this to work
+                return send_file(path+hash_hex+".xlsx", as_attachment=True)
+                # return send_from_directory(path, hash_hex+".xlsx")
+            else:
+                return "Invalid JSON", 400
+        except Exception as e:
+            return str(e), 500
         
         
 
