28665b9b677066c36e2e7101cab20a18fb6d0065
hlu750 <hlu750@aucklanduni.ac.nz>
2023-08-15 19:59:44 +1200

    Added user login modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,65 @@
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Detection from './Pages/detection/Detection';
 import Navbar from './Components/Navbar';
+import LoginModal from './Components/LoginModal';
+
+function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div>
+      <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={openModal}>
+        Login
+      </button>
+
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <LoginModal onClose={closeModal} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="w-full h-screen bg-white">
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/upload" element={<Detection />} />
+        <Route path="/login" element={<LoginModal />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+
+// import { Route, Routes } from 'react-router-dom';
+// import Detection from './Pages/detection/Detection';
+// import Navbar from './Components/Navbar';
+
+// function App() {
+//   return (
+//     <div className="w-full h-screen bg-white">
+//       <Navbar />
+//       <Routes>
+//         <Route path="/upload" element={<Detection />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,15 @@ module.exports = {
     extend: {
       fontFamily: {
         'varela': ['Varela Round', 'sans-serif'],
-      }
+      },
+      colors: {
+        darkslategray: '#2F4F4F',
+        mediumseagreen: '#3CB371',
+        gray: '#808080',
+        lightgray: '#D3D3D3',
+        whitesmoke: '#F5F5F5',
+        // ... other color definitions
+      },
     },
   },
   plugins: [require("daisyui")],
