e5b1ff1ec11a36c1ec65051cbfc2238e9c041c86
JCHU634 <87676426+jchu634@users.noreply.github.com>
2023-08-16 19:53:15 +1200

    [FIX] apiURL declaration fix

--- frontend/src/services/apiService.ts
+++ frontend/src/services/apiService.ts
@@ -2,10 +2,11 @@ import axios from 'axios';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Prediction } from '../models/Prediction';
 
+let apiUrl = '';
 if (process.env.REACT_APP_API_URL) {
-  const apiUrl = process.env.REACT_APP_API_URL;
+  apiUrl = process.env.REACT_APP_API_URL;
 } else {
-  const apiUrl = "http://code-critters.onrender.com/";
+  apiUrl = "http://code-critters.onrender.com/";
 }
 
 export const getPredictions = (formData: FormData) => axios.post<Prediction[]>(apiUrl + 'api/utilities_api/upload_json', formData, {
