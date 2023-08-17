504f3f26afef22f069550a5e12d24bd7bcb781da
KeiHara <84754523+KeiHara@users.noreply.github.com>
2023-08-17 16:41:43 +1200

    changing font

--- frontend/src/Components/LoginModal.tsx
+++ frontend/src/Components/LoginModal.tsx
@@ -27,7 +27,7 @@ function LoginModal() {
       <dialog ref={modalRef} className="modal modal-bottom sm:modal-middle">
         <form method="dialog" className="modal-box grid lg:grid-cols-[1fr_1.5fr] p-0 w-full  sm:w-11/12 sm:max-w-6xl bg-white lg:bg-gradient-to-br lg:from-green-400 lg:to-cyan-500 lg:to-60%">
           <div className="relative hidden lg:flex flex-col items-start justify-center px-14">
-            <div className="text-3xl text-white mb-52">
+            <div className="text-3xl text-white mb-52 font-varela">
               Welcome back!
               <br />
               Let&apos;s continue.
@@ -42,10 +42,10 @@ function LoginModal() {
             >
               <FontAwesomeIcon icon={faXmark} />
             </button>
-            <div className="text-3xl font-bold text-green-500">
+            <div className="text-4xl font-black text-green-500 font-varela">
               Login
             </div>
-            <button className="btn btn-outline w-full text-neutral-600  border-neutral-300 hover:bg-neutral-200 hover:text-neutral-600 hover:border-neutral-300" type="button">
+            <button className="font-varela btn btn-outline w-full text-neutral-600  border-neutral-300 hover:bg-neutral-200 hover:text-neutral-600 hover:border-neutral-300" type="button">
               <img
                 alt="google icon"
                 src="/logos/google.svg"
@@ -53,7 +53,7 @@ function LoginModal() {
               />
               Login with Google
             </button>
-            <button className="btn btn-outline w-full text-neutral-600  border-neutral-300 hover:bg-neutral-200 hover:text-neutral-600 hover:border-neutral-300" type="button">
+            <button className="font-varela btn btn-outline w-full text-neutral-600  border-neutral-300 hover:bg-neutral-200 hover:text-neutral-600 hover:border-neutral-300" type="button">
               <img
                 className="h-3/4"
                 alt="facebook icon"
@@ -62,7 +62,7 @@ function LoginModal() {
 
               Login with Facebook
             </button>
-            <button className="btn btn-outline w-full text-neutral-600  border-neutral-300 hover:bg-neutral-200 hover:text-neutral-600 hover:border-neutral-300" type="button">
+            <button className="font-varela btn btn-outline w-full text-neutral-600  border-neutral-300 hover:bg-neutral-200 hover:text-neutral-600 hover:border-neutral-300" type="button">
               <img
                 className="h-3/4"
                 alt="github icon"
@@ -70,23 +70,23 @@ function LoginModal() {
               />
               Login with Github
             </button>
-            <div className="divider text-neutral-400 before:bg-neutral-200 after:bg-neutral-200">OR</div>
+            <div className="font-varela divider text-neutral-400 before:bg-neutral-200 after:bg-neutral-200">OR</div>
             <div className="w-full">
               {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-              <input type="text" placeholder="Enter your email" className="input input-ghost w-full bg-neutral-100  text-neutral-500 focus:text-neutral-600" />
+              <input type="text" placeholder="Enter your email" className="font-varela input input-ghost w-full bg-neutral-100  text-neutral-500 focus:text-neutral-600" />
             </div>
             <div className="w-full">
               {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-              <input type="text" placeholder="Enter your pasword" className="input input-ghost w-full bg-neutral-100  text-neutral-500 focus:text-neutral-600" />
+              <input type="text" placeholder="Enter your pasword" className="font-varela input input-ghost w-full bg-neutral-100  text-neutral-500 focus:text-neutral-600" />
             </div>
-            <button className="btn w-full text-white text-lg bg-gradient-to-r from-green-400 to-cyan-500 mt-4" type="button">
+            <button className="font-varela btn w-full text-white text-lg bg-gradient-to-r from-green-400 to-cyan-500 mt-4" type="button">
               Login
             </button>
-            <div className="text-neutral-500">
+            <div className="text-neutral-500 font-varela">
               {/* eslint-disable-next-line react/no-unescaped-entities */}
               Don't have an account?
               {' '}
-              <button type="button" className="cursor-pointer text-green-500">Sign up</button>
+              <button type="button" className="font-varela cursor-pointer text-green-500">Sign up</button>
             </div>
           </div>
         </form>
