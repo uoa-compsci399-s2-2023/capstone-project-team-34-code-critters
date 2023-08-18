580a632f96ca5984b6abb714645fdcc2a408037c
KeiHara <84754523+KeiHara@users.noreply.github.com>
2023-08-18 14:12:05 +1200

    restyling detection page

--- frontend/src/Pages/detection/Detection.tsx
+++ frontend/src/Pages/detection/Detection.tsx
@@ -8,7 +8,7 @@ function Detection() {
   const inputFile = useRef<HTMLInputElement>(null);
   const [selectedImages, setSelectedImages] = useState<File[]>([]);
   const [selectedImageUrls, setSelectedImageUrls] = useState<string[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean[]>([]);
   const [predictions, setPredictions] = useState<Prediction[]>([]);
   const [numToShow, setNumToShow] = useState(5);
 
@@ -25,28 +25,41 @@ function Detection() {
     inputFile.current!.click();
   };
 
+  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedImages([...selectedImages, ...Array.from(event.target.files!)]);
+    setIsLoading([...isLoading, ...Array.from(event.target.files!).map(() => false)]);
+  };
+
   useEffect(() => {
     if (selectedImages.length < 1) return;
     const newImageUrls: string[] = [];
     selectedImages.forEach((image:any) => newImageUrls.push(URL.createObjectURL(image)));
     setSelectedImageUrls(newImageUrls);
+    const loadingIndexes: number[] = [];
+    (async () => {
+      const formData = new FormData();
+      selectedImages.forEach((imageUrl, i) => {
+        if (predictions[i] !== undefined) return;
+        formData.append('file[]', imageUrl);
+        setIsLoading((prev) => {
+          const newPrev = [...prev];
+          newPrev[i] = true;
+          return newPrev;
+        });
+        loadingIndexes.push(i);
+      });
+
+      const response = await getPredictions(formData); // Assuming getPredictions is defined
+      setPredictions((prev) => [...prev, ...response.data]);
+      setIsLoading((prev) => {
+        const newPrev = [...prev];
+        loadingIndexes.forEach((i) => {
+          newPrev[i] = false;
+        });
+        return newPrev;
+      });
+    })();
   }, [selectedImages]);
-
-  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedImages([...selectedImages, ...Array.from(event.target.files!)]);
-  };
-
-  const onPredictionClick = async () => {
-    setIsLoading(true);
-    const formData = new FormData();
-    selectedImages.forEach((imageUrl) => {
-      formData.append('file[]', imageUrl);
-    });
-
-    const response = await getPredictions(formData); // Assuming getPredictions is defined
-    setPredictions(response.data);
-    setIsLoading(false);
-  };
 
   const deleteImage = (index: number) => {
     const newImages = [...selectedImages];
@@ -78,7 +91,7 @@ function Detection() {
   };
 
   return (
-    <div className="bg-gray-200 w-full h-full flex justify-center overflow-y-auto">
+    <div className="w-full h-full flex justify-center overflow-y-auto">
       <div className="card max-w-6xl w-11/12 bg-white flex items-center px-4 py-10 my-10 h-fit">
         <h1 className="text-black text-xl font-varela text-center">Drag and Drop or Browse to Upload Image</h1>
         <p className="text-gray-500 mt-4 font-varela text-center">Upload up to 40 images at once</p>
@@ -105,10 +118,10 @@ function Detection() {
             </p>
           </div>
           <button
-            className={`btn btn-outline btn-accent ${selectedImages.length === 0 && 'mt-10'} font-varela`}
+            className={`btn btn-outline btn-primary ${selectedImages.length === 0 && 'mt-10'} font-varela`}
             type="button"
           >
-            {selectedImages.length > 0 ? 'Add more images' : 'Detection images'}
+            {selectedImages.length > 0 ? 'Add more images' : 'Upload images'}
           </button>
         </div>
         <div className="mt-4 w-full flex flex-col gap-4">
@@ -122,22 +135,21 @@ function Detection() {
               <div />
 
               <div className="flex items-center gap-4">
-                {(predictions[index]) ? (
-                  <button
-                    type="button"
-                    className="btn btn-accent btn-square"
-                    onClick={() => openModel(index)}
-                    disabled={isLoading}
-                  >
-                    <FontAwesomeIcon icon={faChartBar} />
-                  </button>
-                ) : <div className="w-12" />}
+                <button
+                  type="button"
+                  className="btn btn-primary btn-square"
+                  onClick={() => openModel(index)}
+                  disabled={isLoading[index]}
+                >
+                  {isLoading[index] ? <span className="loading loading-spinner" />
+                    : <FontAwesomeIcon icon={faChartBar} />}
+                </button>
 
                 <button
                   className="btn btn-square btn-outline btn-accent"
                   type="button"
                   onClick={() => deleteImage(index)}
-                  disabled={isLoading}
+                  disabled={isLoading[index]}
                 >
                   <FontAwesomeIcon icon={faXmark} />
                 </button>
@@ -146,22 +158,6 @@ function Detection() {
             </div>
           ))}
         </div>
-        {
-                    selectedImages.length > 0 && (
-                    <button
-                      onClick={() => onPredictionClick()}
-                      type="button"
-                      className={`btn btn-outline btn-accent ${isLoading && 'btn-active'} mt-4`}
-                      disabled={isLoading}
-                    >
-                      Get Predictions
-
-                      {isLoading && (
-                      <span className="loading loading-spinner" />
-                      )}
-                    </button>
-                    )
-                }
       </div>
 
       {predictions.map((prediction, index) => (
@@ -187,10 +183,10 @@ function Detection() {
                     <div key={i} className="w-full">
                       <div className="flex justify-between">
                         <p className="font-varela text-black">{pred[1]}</p>
-                        <p className="font-varela text-accent">{Number(pred[0]).toFixed(4)}</p>
+                        <p className="font-varela text-primary">{Number(pred[0]).toFixed(4)}</p>
                       </div>
                       <progress
-                        className="progress progress-accent w-full"
+                        className="progress progress-primary w-full"
                         value={Number(pred[0]) * 100}
                         max="100"
                       />
@@ -201,7 +197,7 @@ function Detection() {
                                 prediction.pred.length > numToShow ? (
                                   <button
                                     type="button"
-                                    className="btn btn-accent w-fit"
+                                    className="btn btn-primary w-fit"
                                     onClick={() => handleShowMore(prediction.pred)}
                                   >
                                     Show more
@@ -210,7 +206,7 @@ function Detection() {
                                   : (
                                     <button
                                       type="button"
-                                      className="btn btn-accent w-fit"
+                                      className="btn btn-primary w-fit"
                                       onClick={() => handleShowLess()}
                                     >
                                       Show less
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,5 +10,8 @@ module.exports = {
       }
     },
   },
+  daisyui: {
+    themes: ['emerald']
+  },
   plugins: [require("daisyui")],
 }
