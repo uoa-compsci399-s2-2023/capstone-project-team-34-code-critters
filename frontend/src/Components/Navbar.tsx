087952fbc3a379f026fe54869d2a839cce3ea566
hlu750 <hlu750@aucklanduni.ac.nz>
2023-09-16 18:03:03 +1200

    lint fix

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -51,9 +51,10 @@ function Navbar({ loginModalRef }: NavbarProps) {
       <div className="navbar max-w-4xl rounded-xl w-11/12 fixed z-10 left-1/2 -translate-x-1/2 top-4 shadow backdrop-blur-sm">
         <div className="navbar-start">
           <div className="dropdown dropdown-hover">
-            <label tabIndex={0} className="btn btn-ghost">
-              <FontAwesomeIcon size="2xl" icon={faBurger} />
-            </label>
+            {/* eslint-disable-next-line max-len */}
+            {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex,jsx-a11y/label-has-associated-control */}
+            <label tabIndex={0} className="btn btn-ghost"><FontAwesomeIcon size="2xl" icon={faBurger} /></label>
+            {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
             <ul tabIndex={0} className="dropdown-content z-10 menu p-2 bg-base-100 rounded-lg shadow">
               <li>
                 <button type="button" onClick={() => navigate('/')}>
@@ -74,6 +75,7 @@ function Navbar({ loginModalRef }: NavbarProps) {
         <div className="navbar-end gap-2">
           {user ? (
             <div className="dropdown dropdown-hover">
+              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
               <label className="btn btn-ghost">
                 <FontAwesomeIcon size="2x" icon={faUser} />
               </label>
@@ -89,12 +91,7 @@ function Navbar({ loginModalRef }: NavbarProps) {
                   </button>
                 </li>
                 <li>
-                  <button
-                    type="button"
-                    onClick={() => {
-                      auth.signOut();
-                    }}
-                  >
+                  <button type="button" onClick={() => { auth.signOut(); }}>
                     Log Out
                   </button>
                 </li>
@@ -108,6 +105,7 @@ function Navbar({ loginModalRef }: NavbarProps) {
               className="btn btn-ghost"
               type="button"
             >
+              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
               <label className="swap">
                 <input type="checkbox" checked={isLoginButtonHovered} />
                 <FontAwesomeIcon className="swap-on" icon={faDoorOpen} size="2xl" />
