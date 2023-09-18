612f9ca3548d988b4c21c6fbb6238f22cc501483
KeiHara <84754523+KeiHara@users.noreply.github.com>
2023-09-18 13:48:27 +1200

    fixing layout

--- frontend/src/Pages/home/Home.tsx
+++ frontend/src/Pages/home/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className="pt-28 w-full h-full flex justify-center items-center pt-28">
+    <div className="w-full h-full flex justify-center items-center pt-28">
       <h1 className="text-lg font-varela">Not implemented</h1>
     </div>
   );
