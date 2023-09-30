dd633fe84f2db999abf325ab8728d177a16e20c2
rukun aaron <raar518@aucklanduni.ac.nz>
2023-09-30 20:44:51 +1300

    removed redundant icon

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../enviroments/firebase';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faMagnifyingGlass, faArrowRightToFile, faUserAlt, faRightToBracket} from '@fortawesome/free-solid-svg-icons';
+import { faHome, faMagnifyingGlass, faUserAlt, faRightToBracket} from '@fortawesome/free-solid-svg-icons';
 import 'firebase/auth';
 
 interface NavbarProps {
