70436e16f24c50192302dfc9d62d3fdb33a7e826
JCHU634 <87676426+jchu634@users.noreply.github.com>
2023-10-02 18:13:54 +1300

    Revert "[FIX] Added useragent check to build"
    This reverts commit 069506d86143c6667fec54f25a2f132d38273c8b.

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -36,15 +36,6 @@ function Navbar({ loginModalRef, signUpModalRef, setToastMessage }: NavbarProps)
   }, []);
 
   const navbarEnabled = process.env.REACT_APP_DISABLE_NAVBAR !== 'true';
-  let loginEnabled;
-
-  const userAgent = navigator.userAgent.toLowerCase();
-  if (userAgent.indexOf('client_app') > -1) {
-    loginEnabled = false;
-  } else {
-    loginEnabled = true;
-  }
-  
 
   const handleLogout = async () => {
     try {
@@ -56,148 +47,103 @@ function Navbar({ loginModalRef, signUpModalRef, setToastMessage }: NavbarProps)
   };
 
   if (navbarEnabled) {
-    if (loginEnabled){
-      return (
-        <div className="navbar z-10 rounded-xl w-11/12 fixed left-1/2 -translate-x-1/2 top-4 shadow backdrop-blur-sm max-w-4xl">
-          <div className="flex-1 flex gap-2">
-            <div className="cursor-pointer" onClick={() => navigate('/')}>
-              <img src="/logos/logoV2.svg" alt="logo" />
-            </div>
-            <div className="cursor-pointer" onClick={() => navigate('/')}>
-              <h1 className="font-varela text-2xl font-bold">
-                Code
-                <span className="bg-clip-text from-primary to-secondary bg-gradient-to-r text-transparent">Critters</span>
-              </h1>
-            </div>
+    return (
+      <div className="navbar z-10 rounded-xl w-11/12 fixed left-1/2 -translate-x-1/2 top-4 shadow backdrop-blur-sm max-w-4xl">
+        <div className="flex-1 flex gap-2">
+          <div className="cursor-pointer" onClick={() => navigate('/')}>
+            <img src="/logos/logoV2.svg" alt="logo" />
           </div>
-          <div className="flex md:hidden">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="drawer" aria-label="open sidebar" className="btn btn-square btn-ghost">
-              <FontAwesomeIcon icon={faBars} size="2x" />
-            </label>
+          <div className="cursor-pointer" onClick={() => navigate('/')}>
+            <h1 className="font-varela text-2xl font-bold">
+              Code
+              <span className="bg-clip-text from-primary to-secondary bg-gradient-to-r text-transparent">Critters</span>
+            </h1>
           </div>
-          <div className="md:flex gap-2 hidden">
-            <button
-              className="font-varela btn hover:bg-transparent btn-ghost transition-all border-none relative before:content-[''] before:absolute before:left-0 before:top-0 before:w-0 before:h-full before:bg-gradient-to-br before:from-primary before:to-secondary hover:before:w-full before:-z-10 before:transition-all before:duration-300 duration-300 before:rounded-lg hover:text-white"
-              type="button"
-              onClick={() => navigate('/')}
-            >
-              Home
-              <FontAwesomeIcon icon={faHome} className="-mt-[0.2rem]" />
-  
-            </button>
-            <button
-              className="font-varela btn hover:bg-transparent btn-ghost transition-all border-none relative before:content-[''] before:absolute before:left-0 before:top-0 before:w-0 before:h-full before:bg-gradient-to-br before:from-primary before:to-secondary hover:before:w-full before:-z-10 before:transition-all before:duration-300 duration-300 before:rounded-lg hover:text-white"
-              type="button"
-              onClick={() => navigate('/upload')}
-            >
-              Detect
-              <FontAwesomeIcon icon={faMagnifyingGlass} />
-  
-            </button>
-            {user ? (
-              <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
-                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-                <label className="btn btn-ghost">
-                  {user.photoURL ? (
-                    <img className="w-10 rounded-full" src={user.photoURL} alt="User avatar" />
-                  ) : (
-                    <div className="bg-neutral-focus text-neutral-content w-10 aspect-square rounded-full flex justify-center items-center">
-                      <span>{user.email ? user.email.charAt(0).toUpperCase() : 'A'}</span>
-                    </div>
-                  )}
-                </label>
-                <ul className="dropdown-content text-primary z-10 menu p-2 bg-base-100 rounded-lg shadow">
-                  <li>
-                    <button
-                      type="button"
-                      onClick={() => {
-                        // Handle user history
-                      }}
-                      className="text-black font-varela"
-                    >
-                      User History
-                    </button>
-                  </li>
-                  <li>
-                    <button type="button" className="text-black font-varela" onClick={handleLogout}>
-                      Log Out
-                    </button>
-                  </li>
-                </ul>
-              </div>
-            ) : (
-              <div className="flex gap-2">
-                <button
-                  onClick={openLoginModal}
-                  className="font-varela btn hover:bg-transparent btn-ghost transition-all border-none relative before:content-[''] before:absolute before:left-0 before:top-0 before:w-0 before:h-full before:bg-gradient-to-br before:from-primary before:to-secondary hover:before:w-full before:-z-10 before:transition-all before:duration-300 duration-300 before:rounded-lg hover:text-white"
-                  type="button"
-                >
-                  Login
-                  <FontAwesomeIcon icon={faRightToBracket} />
-  
-                </button>
-                <button
-                  type="submit"
-                  className="font-varela relative border-none btn text-white bg-gradient-to-tl from-primary to-secondary"
-                  onClick={openSignUpModal}
-                >
-                  <div className="font-varela opacity-0 hover:opacity-100 transition duration-500 absolute inset-0 h-full rounded-lg flex justify-center items-center bg-gradient-to-br from-primary to-secondary">
-                    Sign Up
-                    <FontAwesomeIcon icon={faUserAlt} className="pl-2" />
+        </div>
+        <div className="flex md:hidden">
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="drawer" aria-label="open sidebar" className="btn btn-square btn-ghost">
+            <FontAwesomeIcon icon={faBars} size="2x" />
+          </label>
+        </div>
+        <div className="md:flex gap-2 hidden">
+          <button
+            className="font-varela btn hover:bg-transparent btn-ghost transition-all border-none relative before:content-[''] before:absolute before:left-0 before:top-0 before:w-0 before:h-full before:bg-gradient-to-br before:from-primary before:to-secondary hover:before:w-full before:-z-10 before:transition-all before:duration-300 duration-300 before:rounded-lg hover:text-white"
+            type="button"
+            onClick={() => navigate('/')}
+          >
+            Home
+            <FontAwesomeIcon icon={faHome} className="-mt-[0.2rem]" />
+
+          </button>
+          <button
+            className="font-varela btn hover:bg-transparent btn-ghost transition-all border-none relative before:content-[''] before:absolute before:left-0 before:top-0 before:w-0 before:h-full before:bg-gradient-to-br before:from-primary before:to-secondary hover:before:w-full before:-z-10 before:transition-all before:duration-300 duration-300 before:rounded-lg hover:text-white"
+            type="button"
+            onClick={() => navigate('/upload')}
+          >
+            Detect
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+
+          </button>
+          {user ? (
+            <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
+              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+              <label className="btn btn-ghost">
+                {user.photoURL ? (
+                  <img className="w-10 rounded-full" src={user.photoURL} alt="User avatar" />
+                ) : (
+                  <div className="bg-neutral-focus text-neutral-content w-10 aspect-square rounded-full flex justify-center items-center">
+                    <span>{user.email ? user.email.charAt(0).toUpperCase() : 'A'}</span>
                   </div>
+                )}
+              </label>
+              <ul className="dropdown-content text-primary z-10 menu p-2 bg-base-100 rounded-lg shadow">
+                <li>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      // Handle user history
+                    }}
+                    className="text-black font-varela"
+                  >
+                    User History
+                  </button>
+                </li>
+                <li>
+                  <button type="button" className="text-black font-varela" onClick={handleLogout}>
+                    Log Out
+                  </button>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <div className="flex gap-2">
+              <button
+                onClick={openLoginModal}
+                className="font-varela btn hover:bg-transparent btn-ghost transition-all border-none relative before:content-[''] before:absolute before:left-0 before:top-0 before:w-0 before:h-full before:bg-gradient-to-br before:from-primary before:to-secondary hover:before:w-full before:-z-10 before:transition-all before:duration-300 duration-300 before:rounded-lg hover:text-white"
+                type="button"
+              >
+                Login
+                <FontAwesomeIcon icon={faRightToBracket} />
+
+              </button>
+              <button
+                type="submit"
+                className="font-varela relative border-none btn text-white bg-gradient-to-tl from-primary to-secondary"
+                onClick={openSignUpModal}
+              >
+                <div className="font-varela opacity-0 hover:opacity-100 transition duration-500 absolute inset-0 h-full rounded-lg flex justify-center items-center bg-gradient-to-br from-primary to-secondary">
                   Sign Up
-                  <FontAwesomeIcon icon={faUserAlt} />
-                </button>
-              </div>
-            )}
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div className="navbar z-10 rounded-xl w-11/12 fixed left-1/2 -translate-x-1/2 top-4 shadow backdrop-blur-sm max-w-4xl">
-          <div className="flex-1 flex gap-2">
-            <div className="cursor-pointer" onClick={() => navigate('/')}>
-              <img src="/logos/logoV2.svg" alt="logo" />
+                  <FontAwesomeIcon icon={faUserAlt} className="pl-2" />
+                </div>
+                Sign Up
+                <FontAwesomeIcon icon={faUserAlt} />
+              </button>
             </div>
-            <div className="cursor-pointer" onClick={() => navigate('/')}>
-              <h1 className="font-varela text-2xl font-bold">
-                Code
-                <span className="bg-clip-text from-primary to-secondary bg-gradient-to-r text-transparent">Critters</span>
-              </h1>
-            </div>
-          </div>
-          <div className="flex md:hidden">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="drawer" aria-label="open sidebar" className="btn btn-square btn-ghost">
-              <FontAwesomeIcon icon={faBars} size="2x" />
-            </label>
-          </div>
-          <div className="md:flex gap-2 hidden">
-            <button
-              className="font-varela btn hover:bg-transparent btn-ghost transition-all border-none relative before:content-[''] before:absolute before:left-0 before:top-0 before:w-0 before:h-full before:bg-gradient-to-br before:from-primary before:to-secondary hover:before:w-full before:-z-10 before:transition-all before:duration-300 duration-300 before:rounded-lg hover:text-white"
-              type="button"
-              onClick={() => navigate('/')}
-            >
-              Home
-              <FontAwesomeIcon icon={faHome} className="-mt-[0.2rem]" />
-  
-            </button>
-            <button
-              className="font-varela btn hover:bg-transparent btn-ghost transition-all border-none relative before:content-[''] before:absolute before:left-0 before:top-0 before:w-0 before:h-full before:bg-gradient-to-br before:from-primary before:to-secondary hover:before:w-full before:-z-10 before:transition-all before:duration-300 duration-300 before:rounded-lg hover:text-white"
-              type="button"
-              onClick={() => navigate('/upload')}
-            >
-              Detect
-              <FontAwesomeIcon icon={faMagnifyingGlass} />
-  
-            </button>
-          </div>
+          )}
         </div>
-      );
-    }
-    
+      </div>
+    );
   }
   return null;
 }
