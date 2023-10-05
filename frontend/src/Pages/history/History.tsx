347323e027a241ea7cf4dd6d63f890060e6b94ca
lance.n <lnil547@aucklanduni.ac.nz>
2023-10-06 01:14:42 +1300

    Table display and top 3 preds

--- frontend/src/Pages/history/History.tsx
+++ frontend/src/Pages/history/History.tsx
@@ -7,9 +7,24 @@ import React, {
   import { doc, collection, getDoc, getDocs } from 'firebase/firestore';
   import { User } from '@firebase/auth';
 
+
+  type PredictionType = {
+    name: string;
+    date: Date;
+    prediction: string;
+  };
+  
   function History(){
     const [user, setUser] = useState(auth.currentUser);
+    const [predictions, setPredictions] = useState<PredictionType[]>([]);
+
+  const getTopThree = (predictionStr: string): [string, string][] => {
+      const predictionsList: [string, string][] = JSON.parse(predictionStr);
+      return predictionsList.sort((a, b) => parseFloat(b[0]) - parseFloat(a[0])).slice(0, 3);
+    }
     
+    
+
     useEffect(() => {
       auth.onAuthStateChanged((currentUser) => {
         setUser(currentUser);
@@ -27,27 +42,68 @@ import React, {
         try {
           const querySnapshot = await getDocs(predictionsCollectionRef);
 
-          const fetchedPredictions = querySnapshot.docs.map((doc) => doc.data());
-          // setPredictions(fetchedPredictions);
-          console.log(fetchedPredictions);
+          const fetchedPredictions = querySnapshot.docs.map((doc): PredictionType => {
+            const data = doc.data();
+            console.log("Converted date:", data.date.toDate());  // Debugging line
+            return {
+              name: data.name,
+              date: data.date.toDate(),  // Convert the Timestamp to a JavaScript Date object
+              prediction: data.prediction
+            };
+          });
+          
+          setPredictions(fetchedPredictions);
+          console.log(fetchedPredictions,'fetched pred');
         } catch (error) {
           console.error('Error getting predictions:', error);
         }
       } else {
         // Clear predictions when the user logs out
-        // setPredictions([]);
+        setPredictions([]);
       }
     };
 
     fetchPredictions();
   }, [user]);
     
-    return (
-        <div className="w-full h-full flex justify-center overflow-y-auto pt-28 pb-4">
-            
-            
-        </div>
-    )
-  }
+  return (
+    <div className="justify-center overflow-y-auto pt-40 pb-4">
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Date</th>
+            <th>Filename</th>
+            <th>Predictions</th>
+          </tr>
+        </thead>
+        <tbody>
+       {predictions.map((prediction, index) => {
+       const topThreePredictions = getTopThree(prediction.prediction);
+        
+        return (
+          <tr key={index} className="hover:bg-gray-200">
+            <th>{index + 1}</th>
+            <td>{new Date(prediction.date).toLocaleString()}</td>
+            <td>{prediction.name}</td>
+            <td>
+              {topThreePredictions.map((pred, idx) => (
+                <span 
+                  key={idx} 
+                  className={`inline-block transition duration-300 ease-in-out rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 
+                              hover:bg-blue-700 hover:text-white
+                              ${idx === 0 ? 'bg-blue-500' : idx === 1 ? 'bg-blue-400' : 'bg-blue-300'}`}>
+                  {pred[1]}: {parseFloat(pred[0]).toFixed(2)}%
+                </span>
+              ))}
+            </td>
+          </tr>
+        );
+      })}
+    </tbody>
+      </table>
+    </div>
+  );
+}
 
-  export default History;  
+  export default History;
