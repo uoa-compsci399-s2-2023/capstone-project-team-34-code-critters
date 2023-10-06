7c94af86c546ac061ced1ef39af086e2dd71e17f
JCHU634 <87676426+jchu634@users.noreply.github.com>
2023-10-07 02:53:54 +1300

    [NEW] configs for logging

--- backend/asgi.py
+++ backend/asgi.py
@@ -4,4 +4,4 @@ from library import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run("asgi:app", reload=True, port=6789)
+    uvicorn.run("asgi:app", reload=True, port=6789, log_config="log.ini")
