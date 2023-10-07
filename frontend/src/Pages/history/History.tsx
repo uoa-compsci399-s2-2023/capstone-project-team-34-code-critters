8e54efdb87fc8640f4f3c5908b382aed1304ba9a
KeiHara <84754523+KeiHara@users.noreply.github.com>
2023-10-08 01:21:55 +1300

    restyling paginator

--- frontend/src/Pages/history/History.tsx
+++ frontend/src/Pages/history/History.tsx
@@ -7,6 +7,10 @@ import {
 } from 'firebase/firestore';
 import { User } from '@firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faArrowRight, faArrowLeft,
+} from '@fortawesome/free-solid-svg-icons';
 import { auth, db } from '../../enviroments/firebase';
 import { PredictionTable } from '../../models/Prediction';
 import { getImage } from '../../services/apiService';
@@ -14,10 +18,9 @@ import { getImage } from '../../services/apiService';
 function History() {
   const [predictions, setPredictions] = useState<PredictionTable[]>([]);
   const [user] = useAuthState(auth);
-  const [itemsPerPage, setItemsPerPage] = useState(5); 
+  const [itemsPerPage, setItemsPerPage] = useState(5);
   const [currentPage, setCurrentPage] = useState(1);
-  const getTopThree = (prediction: string[][]) =>
-  prediction
+  const getTopThree = (prediction: string[][]) => prediction
     .sort((a, b) => parseFloat(b[0]) - parseFloat(a[0]))
     .slice(0, 3);
   const loadPredictionAndImages = async (currentUser: User) => {
@@ -82,11 +85,11 @@ function History() {
             <tbody>
 
               {displayedPredictions && (
-                displayedPredictions.map((prediction, index)=> {
+                displayedPredictions.map((prediction, index) => {
                   const topThreePredictions = getTopThree(prediction.prediction);
                   return (
                     <tr key={index} className="hover:bg-neutral-100 transition-all ease-in-out duration-300 cursor-pointer">
-                      <td className="w-32" >{prediction.date.toLocaleString()}</td>
+                      <td className="w-32">{prediction.date.toLocaleString()}</td>
 
                       <td>
                         <div className="flex gap-4">
@@ -127,37 +130,11 @@ function History() {
               )}
             </tbody>
           </table>
-          <div className="flex justify-between">
-          <div className="join">
-          <button
-                type="button" 
-                className={`join-item btn ${currentPage === 1 ? 'cursor-not-allowed' : ''}`}
-                onClick={() => handlePageChange(currentPage - 1)}
-                disabled={currentPage === 1}
-              >
-                «
-              </button>
-              <select className="select select-bordered join-item" onChange={(e) => setCurrentPage(Number(e.target.value))} value={currentPage}>
-                {Array.from({ length: totalPages }, (_, i) => (
-                  <option key={i} value={i + 1}>
-                    Page {i + 1}
-                  </option>
-                ))}
-              </select>
-              <button
-                type="button" 
-                className={`join-item btn ${currentPage === totalPages ? 'cursor-not-allowed' : ''}`}
-                onClick={() => handlePageChange(currentPage + 1)}
-                disabled={currentPage === totalPages}
-              >
-                »
-              </button>           
-              </div>
-
-            <div className="flex items-center">
-              <span className="mr-2">Items per page:</span> 
+          <div className="flex justify-end p-4">
+            <div className="join items-center">
+              <span className="mr-2">Items per page:</span>
               <select
-                className="select select-bordered join-item"
+                className="select select-bordered join-item rounded-lg !rounded-l-lg"
                 onChange={(e) => {
                   setItemsPerPage(Number(e.target.value));
                   setCurrentPage(1);
@@ -170,6 +147,31 @@ function History() {
                 <option value={50}>50</option>
                 <option value={100}>100</option>
               </select>
+              <button
+                type="button"
+                className={`join-item btn ${currentPage === 1 ? 'cursor-not-allowed' : ''}`}
+                onClick={() => handlePageChange(currentPage - 1)}
+                disabled={currentPage === 1}
+              >
+                <FontAwesomeIcon icon={faArrowLeft} />
+              </button>
+              <select className="select select-bordered join-item" onChange={(e) => setCurrentPage(Number(e.target.value))} value={currentPage}>
+                {Array.from({ length: totalPages }, (_, i) => (
+                  <option key={i} value={i + 1}>
+                    Page
+                    {' '}
+                    {i + 1}
+                  </option>
+                ))}
+              </select>
+              <button
+                type="button"
+                className={`join-item btn ${currentPage === totalPages ? 'cursor-not-allowed' : ''}`}
+                onClick={() => handlePageChange(currentPage + 1)}
+                disabled={currentPage === totalPages}
+              >
+                <FontAwesomeIcon icon={faArrowRight} />
+              </button>
             </div>
           </div>
 
