928a8926d9f405344cdee0af9ab43ecf012ce6ed
rukun aaron <raar518@aucklanduni.ac.nz>
2023-10-07 23:47:48 +1300

    added  some more items per page options

--- frontend/src/Pages/history/History.tsx
+++ frontend/src/Pages/history/History.tsx
@@ -164,6 +164,8 @@ function History() {
                 <option value={10}>10</option>
                 <option value={15}>15</option>
                 <option value={20}>20</option>
+                <option value={50}>50</option>
+                <option value={100}>100</option>
               </select>
             </div>
           </div>
