d66d2cd9eb742cd001b5b762c31448ec716d41f2
JCHU634 <87676426+jchu634@users.noreply.github.com>
2023-10-15 18:29:27 +1300

    [NEW] Extra model for testing

---
+++ backend/library/models/Unrelated-birds/predict.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+
+async def predict(img, path):
+    model =  tf.saved_model.load(f"{path}/aiy_vision_classifier_birds_V1_1")
+    infer = model.signatures["image_classifier"]
+
+    # Make a prediction
+    output = infer(tf.constant(img))
+
+    # Get the output values    
+    logits = output['logits']
+
+    # Get the predicted class labels
+    top_values, top_indices = tf.math.top_k(logits, k=102)
+
+    # Convert the tensors to Python lists
+    top_values = top_values.numpy().tolist()[0]
+    top_indices = top_indices.numpy().tolist()[0]
+
+    with open(f"{path}/labels.txt", 'r') as file:
+        lines = file.readlines()
+    labels = [line.strip() for line in lines]
+
+    # Keep only top 100
+    values, labels = [], []
+    for value, index in zip(top_values, top_indices):
+        if len(values) < 100:
+            values.append(value)
+            labels.append(labels[index])
+
+    # Pair the top values with their labels
+    predictions_list = [[f'{prob:.20f}', label] for prob, label in zip(values, labels)]
+
+    return predictions_list
---
+++ backend/library/models/Unrelated-birds/preprocess.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+async def img_preprocess(image_path):    
+    # Load the image file and resize it to 224 x 224 pixels
+    img = tf.keras.utils.load_img(image_path, target_size=[224, 224])
+    
+    # Convert the image to an array
+    x = tf.keras.utils.img_to_array(img)
+
+    # Scale the pixel values to [0, 1]
+    x = x / 255.0
+
+    # Preprocess the array for MobileNet
+    x = tf.keras.applications.mobilenet.preprocess_input(x[tf.newaxis,...])
+
+    return x
